77a945f2d429402a4f202a065af4834f18a2b1d6
BrokenSwing <fhugouvieux@gmail.com>
2021-11-06 11:51:17 +0100

    Yet another attempt to make crontask work in production

--- src/main/java/com/github/covidalert/newscraper/config/SchedulerConfig.java
+++ src/main/java/com/github/covidalert/newscraper/config/SchedulerConfig.java
@@ -3,10 +3,25 @@ package com.github.covidalert.newscraper.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
 @EnableScheduling
 @EnableAsync
-public class SchedulerConfig
+public class SchedulerConfig implements SchedulingConfigurer
 {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar)
+    {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("scraper-");
+        scheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(scheduler);
+    }
+
 }
