05248c4afd191d4d1a9cd26e8760563185fb9284
BrokenSwing <fhugouvieux@gmail.com>
2021-11-06 11:42:54 +0100

    Attempt to fix scheduler in production

--- src/main/java/com/github/covidalert/newscraper/MicroserviceTemplateApplication.java
+++ src/main/java/com/github/covidalert/newscraper/MicroserviceTemplateApplication.java
@@ -2,9 +2,10 @@ package com.github.covidalert.newscraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroserviceTemplateApplication
+public class MicroserviceTemplateApplication extends SpringBootServletInitializer
 {
 
     public static void main(String[] args)
